abaeb76fafbee82331d647e297e36cf783ae374f
Martin Bachmann <martinpezbach@gmail.com>
2023-03-04 08:50:51 -0300

    feat: fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url=url, headers={"user-agent": "Fake user-agent"}, timeout=2
+        )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
