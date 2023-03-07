46e40b70f397da6af7267e952634bd96b5ff2d8f
Martin Bachmann <martinpezbach@gmail.com>
2023-03-06 21:25:32 -0300

    feat: top 5 categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
 # Requisito 10
+from collections import Counter
+from tech_news.database import find_news
+
+
 def top_5_categories():
     """Seu código deve vir aqui"""
+    result = []
+    news = find_news()
+    categories = Counter([item["category"] for item in news])
+    for category in sorted(
+        categories.items(), key=lambda item: (-item[1], item[0])
+    ):
+        result.append(category[0])
+    return result
